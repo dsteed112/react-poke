8e18033e8a1734df9f8c747f0b5fcacb67081b35
Daniel Steed <dsteed112@gmail.com>
2020-10-19 14:39:05 -0600

    added loading state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList'
 import axios from "axios"
+import Pagination from './Pagination';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
   const [nextPageUrl, setNextPageUrl] = useState()
   const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
 useEffect(() => {
   axios.get(currentPageUrl).then(res => {
+    setLoading(false)
     setNextPageUrl(res.data.next)
     setPrevPageUrl(res.data.previous)
     setPokemon(res.data.results.map(p => p.name))
   })
 }, [currentPageUrl])
+
+if (loading) return "Loading..."
 
   return (
     <PokemonList pokemon={pokemon}/>
